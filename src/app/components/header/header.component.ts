9b9c4b2c8702b8f11038a8477334fb83162124c8
barbarabarrueta <barbarabarrueta@hotmail.com>
2021-12-22 22:48:29 -0300

    creacion del componente header

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  title: string = 'lista de tareas 2';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
