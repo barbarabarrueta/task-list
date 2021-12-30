c9e050a2b557733386fe843fa245bda23923c420
barbarabarrueta <barbarabarrueta@hotmail.com>
2021-12-29 22:29:53 -0300

    solucionado el conflicto en module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
-<<<<<<< HEAD
-=======
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
->>>>>>> task_list
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ButtonComponent,
-<<<<<<< HEAD
-=======
     TasksComponent,
     TasksItemComponent
->>>>>>> task_list
   ],
   imports: [
     BrowserModule
