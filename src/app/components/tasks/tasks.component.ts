f26349841a729b1644ae726cb7f863ee9a2f3dbd
barbarabarrueta <barbarabarrueta@hotmail.com>
2022-01-06 02:00:38 -0300

    metodo para agregar tareas

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -33,7 +33,13 @@ export class TasksComponent implements OnInit {
 
   toggleReminder(task: Task){
     task.reminder = !task.reminder
-    this.taskService.updateTaskkReminder(task).subscribe;
+    this.taskService.updateTaskkReminder(task).subscribe();
+  }
+
+  addTask(task: Task){
+    this.taskService.addTask(task).subscribe((task)=>(
+      this.tasks.push(task)
+    ))
   }
 
 }
